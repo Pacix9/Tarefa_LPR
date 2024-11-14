13fff0d05cc39f61ed842953761550721d597d62
Pacix9 <146656573+Pacix9@users.noreply.github.com>
2024-11-14 13:04:49 -0300

    treino

---
+++ treino/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class carro
+    {
+        int velocidade;
+        public carro()
+        {
+
+            this.velocidade = 120;
+        }
+        public int vm
+        {
+          get
+            {
+                return this.velocidade; 
+            }
+
+            set
+            {
+                int resposta;
+                resposta = int.Parse(Console.ReadLine());
+                if(resposta < 0)
+                {
+                    velocidade  = 0;
+                }
+                else if(resposta > 300) {
+                    velocidade = 300;
+                
+                }
+                else
+                {
+                    velocidade = resposta;  
+                }
+
+            }
+        }
+
+
+
+
+
+      
+
+    }
+}
---
+++ treino/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1;
+
+namespace Aula_11
+{
+    internal class sla
+    {
+        static void Main(string[] args)
+        {
+
+            carro x = new carro();
+            x.vm = int.Parse(Console.ReadLine());
+            Console.WriteLine(x.vm);
+
+
+
+
+        }
+    }
+}
